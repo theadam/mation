6d1e467c94efed7a8b0df156fe4e62eb22ad699d
Adam Nalisnick <theadam4257@gmail.com>
2016-02-24 12:48:31 -0500

    updating gh-pages

--- dist/mation.js
+++ dist/mation.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["flydSpring"] = factory();
+		exports["mation"] = factory();
 	else
-		root["flydSpring"] = factory();
+		root["mation"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
